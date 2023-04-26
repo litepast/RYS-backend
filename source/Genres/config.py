21d94e7ed19f7a361ffbf2567358a29bf3bf4f3c
litepast <litepast@hotmail.com>
2023-04-25 20:05:49 -0600

    modified:   source/Genres/config.py 	modified:   .gitignore

--- source/Genres/config.py
+++ source/Genres/config.py
@@ -3,8 +3,9 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import discogs_client
 
 mysql_connection='mysql+mysqlconnector://root:Adults2137224!@localhost:3306/rys'
-discogs_token='uytzEONbTVeTThmGsfMAAdmjrvbSzYmMrohodPAE'
+
 client_id = '9a65d633d6cc467ea07bb013b29b4b6a'
+discogs_token='uytzEONbTVeTThmGsfMAAdmjrvbSzYmMrohodPAE'
 client_secret = '5de4eb24942449aeae4f0288247b2b5a'   
 genres_html = 'C:/SideProjects/RateYourSpotify/backend/source/Genres/Genres_and_Styles_Discogs.html'
 
