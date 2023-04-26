0b5115caccc25dcfccc1387110336120a4951b1c
litepast <litepast@hotmail.com>
2023-04-25 20:20:19 -0600

    modified:   source/Genres/config.py

--- source/Genres/config.py
+++ source/Genres/config.py
@@ -9,5 +9,5 @@ client_secret = '5de4eb24942449aeae4f0288247b2b5a'
 genres_html = 'C:/SideProjects/RateYourSpotify/backend/source/Genres/Genres_and_Styles_Discogs.html'
 
 auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(auth_manager=auth_manager)
 d = discogs_client.Client('my_user_agent/1.0', user_token=discogs_token)
+sp = spotipy.Spotify(auth_manager=auth_manager)
