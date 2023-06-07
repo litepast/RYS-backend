4221fde626f381a9b2b749d54ea619f7aef6ec2c
litepast <litepast@hotmail.com>
2023-06-06 22:30:01 -0600

    modified:   Tests/tests.py 	new file:   genres.json 	new file:   source/genres_styles_json.py 	new file:   styles.json

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,17 +1,45 @@
-import requests
-import time
+# import requests
+# import time
 
-url = 'http://127.0.0.1:5000/api/v1/insert-album-catalog/1oNccmu6KxWyQnGi9P7g5u'
+# url = 'http://127.0.0.1:5000/api/v1/insert-album-catalog/1oNccmu6KxWyQnGi9P7g5u'
 
-# Define the JSON data to send in the request body
+# # Define the JSON data to send in the request body
 
-# Send a POST request with the JSON data in the body
-start= time.time()
-response = requests.put(url)
+# # Send a POST request with the JSON data in the body
+# start= time.time()
+# response = requests.put(url)
 
-# Print the response from the server
-print(response.text)
-end = time.time()
-print('tiempo de todo ', end - start)
+# # Print the response from the server
+# print(response.text)
+# end = time.time()
+# print('tiempo de todo ', end - start)
+
+import json
+
+# Sample array of dictionaries
+data = [
+    {
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+    },
+    {
+        "name": "Alice",
+        "age": 25,
+        "city": "San Francisco"
+    },
+    {
+        "name": "Bob",
+        "age": 35,
+        "city": "Seattle"
+    }
+]
+
+# Convert array of dictionaries to JSON string
+json_data = json.dumps(data)
+
+# Write JSON string to a file
+with open("data.json", "w") as json_file:
+    json_file.write(json_data)
 
 
---
+++ source/genres_styles_json.py
@@ -0,0 +1,50 @@
+import models
+import json
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+
+path = 'C:\SideProjects\RateYourSpotify\rys frontend\src\data'
+
+def toJson(): 
+    with Session(models.engine) as session:
+        session.begin()
+        try:
+            genres_columns = ['id', 'name', 'selected','hover'] 
+            genres_object = []
+            genres_query = 'select id, name from genres'
+            genres_data = session.execute(text(genres_query))
+            for row in genres_data:
+                genres_rows = { column: None for column in genres_columns}
+                genres_rows['id'] = row[0]
+                genres_rows['name'] = row[1]
+                genres_rows['selected'] = False
+                genres_rows['hover'] = False
+                genres_object.append(genres_rows)
+
+            styles_columns = ['id', 'name', 'selected','hover'] 
+            styles_object = []
+            styles_query = 'select id, name from styles'
+            styles_data = session.execute(text(styles_query))
+            for row in styles_data:
+                styles_rows = { column: None for column in styles_columns}
+                styles_rows['id'] = row[0]
+                styles_rows['name'] = row[1]
+                styles_rows['selected'] = False
+                styles_rows['hover'] = False
+                styles_object.append(styles_rows)
+            
+
+            with open("genres.json", "w") as outfile:
+                 json.dump(genres_object, outfile)
+
+            with open("styles.json", "w") as outfile:
+                 json.dump(styles_object, outfile)
+
+        except Exception as e:
+            session.rollback()        
+        else:
+            session.commit()
+
+
+toJson()
+        
